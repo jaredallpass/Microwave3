15874b2deb9de6cb4095e158b0b4571ea78f21ce
jaredallpass <jared.allpass@cloudbi.co.za>
2018-03-15 12:30:15 +0200

    A somewhat working Microwave

---
+++ Microwave3/Microwave3.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+public class Microwave
+{
+
+    /// Allows a new microwave to be installed    
+    public Microwave()
+    {       
+    }
+
+    public int min;
+
+    public int minutes
+    {
+        get { return min; }
+        set { min = value; }
+    }
+
+    public int sec;
+
+    public int seconds
+    {
+        get { return sec; }
+        set { sec = value; }
+    }
+
+    public TimeSpan ts;
+
+    public TimeSpan timer
+    {
+        get { return ts; }
+        set { ts = value; }
+    }
+    /// Allows you to set the minutes and seconds of how long your food must be cooked
+    public void SetTimer(TimeSpan ts)
+    {
+        timer = ts;
+    }
+
+    /// <summary>
+    /// ////////////////////////////////////////////////////////////////
+    /// </summary>
+
+    public string food;
+
+    public string foods
+    {
+        get { return food; }
+        set { food = value; }
+    }
+
+    /// Allows you to specify the food you're putting in
+    public void PutFoodIn(string food)
+    {
+        foods = food;
+    }
+
+    public void TakeFoodOut()
+    {
+        food = null;
+        Console.WriteLine("Microwave is now empty");
+    }
+
+    /// <summary>
+    /// ////////////////////////////////////////////////////////////////
+    /// </summary>
+
+    public int temp;
+
+    public int temps
+    {
+        get { return temp; }
+        set { temp = value; }
+    }
+    /// Allows you to set the heat in degrees celsius
+    public void Temperature(int temp)
+    {
+        temps = temp;
+    }
+    /// Allows you to start your Microwave
+    public void Start()
+    {
+        TimeSpan tr = new TimeSpan(0, 0, 0);
+        TimeSpan tm = new TimeSpan(0, 0, 1);
+        Console.WriteLine("Heat: {0}*C", temp);
+        Console.WriteLine("Food: {0}", food);
+        while (ts > tr)
+        {
+            ts = ts - tm;
+
+            Console.SetCursorPosition(0, 2);
+            Console.Write("Time left: {0}", ts);
+            System.Threading.Thread.Sleep(1000);
+        }
+        Console.WriteLine("Enjoy your {0}", food);
+    }
+
+    /// <summary>
+    /// ////////////////////////////////////////////////////////////////
+    /// </summary>
+    
+
+
+}
+
+
+
+
+
+
+
+
--- Microwave3/Program.cs
+++ Microwave3/Program.cs
@@ -10,31 +10,7 @@ namespace Microwave3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to your Microwave!");
-            Console.WriteLine("Put your food in the microwave..");
-            string food = Console.ReadLine();
-            Console.WriteLine("Please enter the amount of minutes:");
-            int min = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter the amount of seconds:");
-            int sec = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter the heat required:");
-            int heat = int.Parse(Console.ReadLine());
-            TimeSpan ts = new TimeSpan(0, min, sec);
-            TimeSpan tr = new TimeSpan(0, 0, 0);
-            TimeSpan tm = new TimeSpan(0, 0, 1);
-            while (ts > tr) {
-                ts = ts - tm;
-                Console.SetCursorPosition(0, 12);
-                Console.Write("Your {1} will be ready in {0} ", ts, food);
-                System.Threading.Thread.Sleep(1000);
-            }
-            Console.WriteLine("\r\nYour food is ready!");
-            Console.WriteLine("Press enter to remove your {0}..", food);
-            string key = Console.ReadLine();
-            if (key == "")
-                Console.WriteLine("Enjoy your {0}!", food);
-            else
-                Console.WriteLine("You did not press enter.");
+
         }
     }
 }
